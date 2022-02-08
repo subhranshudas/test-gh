7a4fd607027ad76920e0fb030b2488e9d262c0ac
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-02-08 19:08:21 +0530

    react env

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@
 import './App.css';
 
 function App() {
+
+  console.log("test--> ", process.env);
+  debugger;
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,6 +15,10 @@ function App() {
           <p>Displaying ENV values: </p>
           <p>JUNK_KEY : {process.env.JUNK_KEY}</p>
           <p>FAKE_SECRET : {process.env.FAKE_SECRET}</p>
+          <hr />
+
+          <p>REACT JUNK_KEY : {process.env.REACT_APP_JUNK_KEY}</p>
+          <p>REACT FAKE_SECRET : {process.env.REACT_APP_FAKE_SECRET}</p>
         </div>
       </header>
     </div>
